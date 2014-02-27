87d4308728754eb9e79c2c9164999e3d9e5279f9
koderok <yadavpulkit@gmail.com>
2014-02-28 00:09:33 +0530

    Tests for titles and body

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
-  
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -14,6 +14,11 @@ describe PagesController do
       response.should have_selector("title", 
                         :content => "Koderok Ruby on Rails Sample App | Home")
     end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
@@ -21,12 +26,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+                        :content => "Koderok Ruby on Rails Sample App | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+                        :content => "Koderok Ruby on Rails Sample App | About")
     end
   end
 
