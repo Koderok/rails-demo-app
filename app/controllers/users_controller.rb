54a0c7949decb874fbca6d4985521d482a70fb91
koderok <yadavpulkit@gmail.com>
2014-03-05 12:37:04 +0530

    User model validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @title = "Sign Up"
   end
+
+  def create
+    User.create(user_params)
+      # @user = User.new(user_params)
+      # if @user.save
+      #   # Handle a successful save.
+      # else
+      #   render 'new'
+      # end
+    end
+
+  private   
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
